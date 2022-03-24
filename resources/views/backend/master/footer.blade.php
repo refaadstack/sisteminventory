6358a2f30e85d214277ee1df20b5432d929a9c34
refaadstack <refaad16@gmail.com>
2022-03-24 13:08:06 +0700

    Starter

---
+++ resources/views/backend/master/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left text-white">
+      Copyright &copy; 2022 Redho Fadillah Adha <div class="bullet"></div></a>
+    </div>
+    <div class="footer-right">
+      2.3.0
+    </div>
+</footer>
