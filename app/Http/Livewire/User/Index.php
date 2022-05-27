22685f7f62f1031d086bd37e73847a4d79a61e9b
refaadstack <refaad16@gmail.com>
2022-05-27 14:22:59 +0700

    user destroy

--- app/Http/Livewire/User/Index.php
+++ app/Http/Livewire/User/Index.php
@@ -11,4 +11,11 @@ class Index extends Component
     {
         return view('livewire.user.index', ['user'=> User::paginate(10)])->extends('backend.master.master')->section('content');
     }
+    public function destroy()
+    {
+        $user = User::find($this->id);
+        $user->delete();
+        session()->flash('message', 'Data berhasil dihapus');
+        return redirect()->route('user.index');
+    }
 }
